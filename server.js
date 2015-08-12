5fb07096616666f662512c7460b764f486ffd2a8
Sebastien Cheraux <scheraux@gmail.com>
2015-08-11 19:35:18 -0700

    Add helper functions for sending 404 errors and serving file data

---
+++ server.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+var cache = {};
+
+function send404(response) {
+  response.writeHead(404, {'Content-Type': 'text/plain'});
+  response.write('Error 404: resource not found.');
+  response.end();
+}
+
+function sendFile(response, filePath, fileContents) {
+  response.writeHead(
+    200,
+    {'Content-Type': mime.lookup(path.basename(filePath))}
+  );
+  response.end(fileContents);
+}
